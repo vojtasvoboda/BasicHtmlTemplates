b203c6213e4c5a36e500ada80174ec6894b21ad4
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2015-10-09 12:00:55 +0200

    Add FullHeight template

---
+++ FullHeightTemplate/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="cs" />
+
+<title>Page Title &#124; Hlavní stránka</title>
+
+<meta name="keywords" content="klicova slova" />
+<meta name="description" content="popis webu" />
+<meta name="copyright" content="Vojta Svoboda; mailto:vojtasvoboda.cz@gmail.com" />
+<meta name="distribution" content="global" />
+<meta name="author" content="Vojta Svoboda; mailto:vojtasvoboda.cz@gmail.com" />
+<meta name="language" content="Czech" />
+<meta name='robots' content='all,follow' />
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="30 days" />
+
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="icon" href="favicon.ico" />
+
+<link rel="stylesheet" href="css/styl.css" type="text/css" media="screen,projection,tv" />
+<link rel="stylesheet" href="css/print.css" type="text/css" media="print" />
+
+<!--[if IE ]>
+<link rel="stylesheet" href="css/onlyie.css" type="text/css" media="screen,projection,tv" />
+<![endif]-->
+
+</head>
+
+<body>
+<div id="wrapper">
+
+  <div id="header">
+
+    <h1>Page title</h1>
+    <h2>Page subtitle</h2>
+    
+    <p class="noscreen">
+      <a href="#nav" title="Přejít na menu">Přejít na menu</a>
+      <span>&ndash;</span>
+      <a href="#content" title="Přejít na obsah">Přejít na obsah</a>
+    </p>
+    
+    <p class="home_email">
+       <a href="/" title="Přejít na domovskou stránku">domů</a>
+       <a href="/kontakty/" title="Napište nám">napište nám</a>
+    </p>
+
+  </div><!-- end of DIV header -->
+
+  <hr class="noscreen" />
+
+  <div id="content">
+  
+      <div id="section">
+        <p>Lorem <a href="#" title="">ipsum dolor sit amet</a>, consectetur adipiscing elit. Donec orci massa, mattis id semper quis, fringilla vel neque. Nunc molestie ullamcorper faucibus. Nam malesuada consectetur lacus, ac convallis dui semper eu. Vestibulum tincidunt massa vel diam ullamcorper vitae pulvinar massa cursus. Duis iaculis adipiscing leo a luctus. Etiam et molestie diam. Aliquam sagittis lacinia suscipit. Etiam lorem ipsum, fringilla ac tristique vel, faucibus a felis. Integer sollicitudin pulvinar dolor, tincidunt porta odio semper egestas. Cras quis tellus eu ante adipiscing feugiat. Aenean adipiscing metus ac nunc facilisis id posuere erat vehicula. Mauris vel justo et lectus lobortis iaculis nec in mauris.</p>
+        <ul>
+          <li>První odrážka</li>
+          <li>Odrážka</li>
+          <li>Položka poslední</li>
+        </ul>
+        <!--
+        <p>Fusce fermentum sodales pulvinar. Nulla volutpat, sem at consectetur fermentum, elit lacus feugiat tortor, eu elementum mauris elit ut justo. Vestibulum ligula sem, euismod sed egestas non, euismod ac mi. Aenean consectetur, lectus vitae rhoncus vestibulum, odio quam pellentesque erat, in commodo enim nunc at eros. Integer porta dolor ut quam consectetur eu facilisis justo viverra. Maecenas dictum fringilla nisl et facilisis. Aenean mi enim, hendrerit ut aliquam sit amet, fermentum sed nunc. Mauris tincidunt elit in sem blandit non pulvinar lorem ornare. Vivamus et sem lectus. Ut malesuada ultricies ultricies. Morbi tortor arcu, lobortis sed fringilla nec, sodales nec dolor.</p>
+        <ol>
+          <li>První odrážka</li>
+          <li>Odrážka</li>
+          <li>Položka poslední</li>
+        </ol>
+        <p>Fusce fermentum sodales pulvinar. Nulla volutpat, sem at consectetur fermentum, elit lacus feugiat tortor, eu elementum mauris elit ut justo. Vestibulum ligula sem, euismod sed egestas non, euismod ac mi. Aenean consectetur, lectus vitae rhoncus vestibulum, odio quam pellentesque erat, in commodo enim nunc at eros. Integer porta dolor ut quam consectetur eu facilisis justo viverra. Maecenas dictum fringilla nisl et facilisis. Aenean mi enim, hendrerit ut aliquam sit amet, fermentum sed nunc. Mauris tincidunt elit in sem blandit non pulvinar lorem ornare. Vivamus et sem lectus. Ut malesuada ultricies ultricies. Morbi tortor arcu, lobortis sed fringilla nec, sodales nec dolor.</p>
+        <p class="right"><a href="" title="">Kontaktujte nás pro více informací&hellip;</a></p>
+        -->
+      </div><!-- end of DIV section -->
+      
+      <div id="aside">
+        <h3>Ukázky našich prací:</h3>
+        <img src="#" alt="" />
+        <img src="#" alt="" />
+        <img src="#" alt="" />
+      </div><!-- end of DIV aside -->
+
+  </div><!-- end of DIV content -->
+  
+  <hr class="noscreen" />
+  
+  <div id="nav">
+      <h3 class="noscreen">Hlavní menu:</h3>
+      <ol>
+        <li><a href="/o-nas/" accesskey="1">O nás</a></li>
+        <li><a href="/reference/" accesskey="2">Reference</a></li>
+        <li><a href="/cenik/" accesskey="3">Ceník</a></li>
+        <li><a href="/poptavka/" accesskey="4">Poptávka</a></li>
+        <li><a href="/kontakt/" accesskey="5">Kontakt</a></li>
+      </ol>
+  </div><!-- end of DIV nav -->
+
+  <hr class="noscreen" />
+  
+  <div id="footer">
+    <p>Copyright &copy; 2015 <a href="http://www.firma.cz/">Supersites</a>, All Rights Reserved</p>
+  </div><!-- end of DIV footer -->    
+  
+</div><!-- end of DIVu wrapper -->
+
+</body>
+</html>
